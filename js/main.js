f24b2a605964c8f1546d5b47a5ab37c2a4de77f8
ka7de <133555923+ka7de@users.noreply.github.com>
2023-08-24 15:47:44 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function(){
     var btn = $('.cart-btn');
     $('#check').on('click',function(){
         if(this.checked){
-            btn.html('<a href="">カートに入れる</a>');
+            btn.html('<a href="cart.php">カートに入れる</a>');
             $('.cart-btn').css('opacity','1');
         } else {
             btn.html('カートに入れる');
